8f11072ca7d710ecc44a2c94195ab6491d05bdaa
gabrielfillus <137187352+gabrielfillus@users.noreply.github.com>
2024-10-17 09:44:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'Brasilia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Argentina?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'História',
+    'Quando marx nasceu?',
+    '1818'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
